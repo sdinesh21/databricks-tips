510b0a364ddbf9ceab54a7cbd3d1e12ea5f85e78
Arthur Luz <arthurjosemberg@gmail.com>
2020-01-11 16:33:15 -0300

    add of format_columns_name.py and pandas_to_spark.py scripts.

---
+++ format_columns_name.py
@@ -0,0 +1,58 @@
+import databricks.koalas as ks
+import pandas as pd
+import re
+
+def get_column_names (dataframe):
+  """
+  Receive a dataframe and return a list with all dataframe columns name.
+  
+  Args:
+    dataframe: dataframe in PyPark, Koalas or Pandas.
+
+  Returns:
+    A list with all dataframe columns name.
+  """
+  columns_list = []
+  columns_list = dataframe.columns
+  return columns_list
+
+def format_columns_name (dataframe):
+  """
+  Receive a dataframe (PySpark, Koalas or Pandas) and return another dataframe 
+  with the same type of the input one with formated columns name.
+  
+  Args:
+    dataframe: dataframe in PyPark, Koalas or Pandas.
+
+  Returns:
+    A dataframe with formated columns name.
+  """
+  column_names = get_column_names(dataframe)
+  special_characters = [['[áãàâäåæ]','[éêèęėēë]','[íîìïįī]','[óõôòöœøō]','[úüùûū]','[ç]','[ñ]','[/\-\n\r.,]','[(){};:]'],
+                        ['a','e','i','o','u','c','n','_','']]
+  amount_of_types = len(special_characters[0])
+  
+  if isinstance(dataframe, (pd.DataFrame,ks.DataFrame)):  
+    
+    for column in column_names:
+      column_name = column
+    
+      for index_regex in range(amount_of_types):
+        column_name = re.sub(special_characters[0][index_regex], special_characters[1][index_regex], column_name) 
+      dataframe.rename(columns={column:column_name}, inplace=True)
+    
+    dataframe.columns = dataframe.columns.str.replace(' ', '_')
+    dataframe.columns = dataframe.columns.str.lower()
+  
+  else:
+    
+    for column in column_names:
+      column_name = column
+      column_name = column_name.lower().replace(' ','_')
+      
+      for index_regex in range(amount_of_types):
+        column_name = re.sub(special_characters[0][index_regex], special_characters[1][index_regex], column_name) 
+      
+      dataframe = dataframe.withColumnRenamed(column,column_name)
+  
+  return dataframe
---
+++ pandas_to_spark.py
@@ -0,0 +1,57 @@
+from pyspark.sql.types import *
+
+def equivalent_type(datatype):
+  """
+  Choose the PySpark datatype that is equivalent to Pandas datatype.
+  
+  Args:
+    datatype: Parameter with string value with Pandas datatype.
+
+  Returns:
+    The PySpark datatype that is equivalent to Pandas datatype passed.
+  """  
+  choices = {
+    'datetime64[ns]': TimestampType(), 
+    'int64': LongType(),
+    'int32': IntegerType(),
+    'float64': FloatType()
+  }
+  return choices.get(datatype, StringType())
+
+def define_structure(string, format_type):
+  """
+  Receive a column name and Pandas datatype and return the a Struct datatype 
+  with the column name, the PySpark datatype and the nullable parameter with 
+  value TRUE.
+
+  Args: 
+    string: Parameter that will pass the column name.
+    format_type: Parameter that will pass the Pandas datatype.
+
+  Returns:
+    The PySpark struct datatype with the column name and a datatype with 
+    PySpark correspondent value.
+  """
+  try: typo = equivalent_type(format_type)
+  except: typo = StringType()
+  return StructField(string, typo)
+
+def pandas_to_spark(pandas_df):
+  """
+  Recive a pandas dataframe and return a schema to be used to create a 
+  PySpark dataframe.
+
+  Args:
+    pandas_df: a Pandas dataframe that will be converted. 
+
+  Returns:
+    A pySpark dataframe with datatypes that correspond correctly to Pandas
+    dataframe that was passed.
+  """
+  columns = list(pandas_df.columns)
+  types = list(pandas_df.dtypes)
+  struct_list = []
+  for column, typo in zip(columns, types): 
+    struct_list.append(define_structure(column, str(typo)))
+    p_schema = StructType(struct_list)
+  return spark.createDataFrame(pandas_df, p_schema)
